8acb3953c3c918e28272020395c80414b67330b8
SABlythe <sblythe@lindenwood.edu>
2022-08-31 12:04:02 -0500

    Add files via upload

---
+++ SimpleFileIO.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ifstream ifile("data.txt");
+
+	if (!ifile)
+	{
+		cout << "File could not be opened for reading !";
+		return 1;
+	}
+
+	double sum = 0;
+	unsigned int numValues=0;
+
+	double nextVal;
+
+	ifile >> nextVal;
+
+	while (ifile)
+	{
+		numValues++;
+		sum += nextVal;
+
+		ifile >> nextVal;
+	}
+
+	cout << "The average of all things is : " << sum/numValues << endl;
+
+	return 0;
+}
