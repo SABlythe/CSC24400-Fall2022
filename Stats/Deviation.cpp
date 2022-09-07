d8af91d4d3df728d3297b0cb7886a83578b7ee22
Stephen Blythe <sblythe@lindenwood.edu>
2022-09-07 12:16:19 -0500

    Completed the Standard Deviation example

--- Stats/Deviation.cpp
+++ Stats/Deviation.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <fstream>
 #include <iostream>
 
@@ -12,8 +13,50 @@ int main(int argc, char* argv[])
 
 	ifstream myStream(fileName); // not compiling? Try fileName.c_str()
 
-	double val;
-	myStream >> val;
-
+	double sum =0;
+	int numValues=0;
+	
+	double values [10000];
+	myStream >> values[numValues];
+	
+	while ( myStream )
+	{
+		sum += values[numValues]; 
+		numValues++;
+		
+		myStream >> values[numValues];
+	}
+	
+	double mean = sum/numValues;
+	
+	double varianceSum=0;
+	
+	for (int index =0; index< numValues; index++) 
+	{
+		double delta = values[index] - mean; 
+		varianceSum += delta * delta;
+	}
+	
+	double stddev = sqrt( varianceSum/(numValues-1) );  
+	 
+	cout << "standard deviation = " << stddev << endl;
+	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
